ec49d1d272f1a231e0897d7ce6fc82d673b87e11
bruce-zxy <bruce_zxy@163.com>
2017-08-16 19:11:32 +0800

    pause

--- src/js/TodoList.js
+++ src/js/TodoList.js
@@ -3,7 +3,20 @@ import { observer } from "mobx-react";
 
 @observer
 export default class TodoList extends Component {
+	filter(e) {
+		this.props.store.filter = e.target.value;
+	}
 	render() {
-		return <h1>{this.props.store.todos[0]}</h1>
+		const { filter, todos } = this.props.store;
+
+		const todoLis = todos.map(todo => (
+			<li>{todo}</li>
+		))
+		return <div>
+				<h1>todos</h1>
+				{filter}
+				<input type="text" className="filter" value={filter} onChange={this.filter.bind(this)} />
+				<ul>{todoLis}</ul>
+			</div>
 	}
 }
